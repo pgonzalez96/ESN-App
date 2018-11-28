575006116299ca63faa11b0589e8c5356e0ecc32
paugm96 <pau.gonzalez.montiel@est.fib.upc.edu>
2018-11-28 03:01:08 +0100

    VOte events

--- src/components/EventInfo/index.js
+++ src/components/EventInfo/index.js
@@ -27,17 +27,83 @@ export default class EventInfo extends React.Component {
             lng: '',
             button: '',
             buttonStyle: '',
-            loading: 0,
+            loading: true,
             stars: 2.5,
-            before: false
+            before: false,
+            voted: false,
+            press: false
         }
 
         this.onAddressClicked = this.onAddressClicked.bind(this);
         this.onButtonPressed = this.onButtonPressed.bind(this);
         this.componentDidMount = this.componentDidMount.bind(this);
+        this.convertMonth = this.convertMonth.bind(this);
+        this.getDate = this.getDate.bind(this);
+        this.updateAverage = this.updateAverage.bind(this);
+        this.onButtonVoteEventPressed = this.onButtonVoteEventPressed.bind(this);
+        this.onButtonVotePressed = this.onButtonVotePressed.bind(this);
     };
 
+    convertMonth(month) {
+        if (month === 'January') {
+            return '01';
+        }
+        else if(month === 'February') {
+            return '02';
+        }
+        else if(month === 'March') {
+            return '03';
+        }
+        else if(month === 'April') {
+            return '04';
+        }
+        else if(month === 'May') {
+            return '05';
+        }
+        else if(month === 'June') {
+            return '06';
+        }
+        else if(month === 'July') {
+            return '07';
+        }
+        else if(month === 'August') {
+            return '08';
+        }
+        else if(month === 'September') {
+            return '09';
+        }
+        else if(month === 'October') {
+            return '10';
+        }
+        else if(month === 'November') {
+            return '11';
+        }
+        else {
+            return '12';
+        }
 
+
+    }
+
+    getDate(date) {
+        let year = date.substring(date.length-4, date.length);
+        let day, month;
+        let space = false;
+        let i = date.length-6;
+        while(!space) {
+            if (date[i] === ' ') {
+                month = date.substring(i+1,date.length-6);
+                day = date.substring(i-2,i);
+                space = true;
+            }
+            --i;
+        }
+        month = this.convertMonth(month);
+        let finalDate = day.concat(month);
+        finalDate = finalDate.concat(year);
+        return finalDate;
+
+    }
 
 
     componentDidMount() {
@@ -55,6 +121,79 @@ export default class EventInfo extends React.Component {
                 place: snapshot.val().place,
                 time: snapshot.val().time
             })
+            let date = new Date().toLocaleDateString();
+            let currentYear = date.substring(6,8);
+            currentYear = Number(currentYear)+2000;
+            let currentMonth = Number(date.substring(0,2));
+            let currentDay = Number(date.substring(3,5));
+            let finalDate = this.getDate(this.state.date);
+            let year = Number(finalDate.substring(4,8));
+            let month = Number(finalDate.substring(2,4));
+            let day = Number(finalDate.substring(0,2));
+            if (year <= currentYear) {
+                if (year < currentYear) {
+                    this.setState({
+                        before: true
+                    });
+                }
+                else {
+                    if (month <= currentMonth) {
+                        if (month < currentMonth) {
+                            this.setState({
+                                before: true
+                            });
+                        }
+                        else {
+                            if (day < currentDay) {
+                                this.setState({
+                                    before: true
+                                });
+                            }
+                        }
+                    }
+                }
+            }
+            if (this.state.before) {
+                rootRef = firebase.database().ref();
+                ref = rootRef.child('users/');
+                let email = currentUser.providerData[0].email;
+                ref.once('value').then(snapshot => {
+                    snapshot.forEach(user => {
+                        if (user.val().email === email) {
+                            if (user.val().votedEvents) {
+                                let usF = ref.child(user.key+'/votedEvents');
+                                usF.once('value').then(snap => {
+                                    snap.forEach(eve => {
+                                        if (eve.val() === this.state.name) {
+                                            this.setState({
+                                                voted: true
+                                            })
+                                        }
+                                    })
+                                    if (!this.state.voted) {
+                                        rootRef = firebase.database().ref();
+                                        ref = rootRef.child('events/'+this.state.name);
+                                        ref.once('value').then(snapshot => {
+                                            if (!snapshot.val().average) {
+                                                let av = rootRef.child('events/'+this.state.name+'/averages');
+                                                av.set(0);
+                                                let usVoted = rootRef.child('events/'+this.state.name+'/votes');
+                                                usVoted.set(0);
+                                            }
+                                        });
+                                    }
+                                })
+
+                            }
+
+
+                        }
+                    })
+                });
+
+
+            }
+
             Geocoder.init('AIzaSyDDIEZf8ns_KZkfcZUwh0R5n5lErf-rk1c'); // use a valid API key
             Geocoder.from(this.state.address + " " + this.state.city)
                 .then(json => {
@@ -106,13 +245,7 @@ export default class EventInfo extends React.Component {
 
             })
         });
-        let date = new Date().toLocaleDateString();
-        let year = date.substring(6,8);
-        let month = date.substring(0,2);
-        let day = date.substring(3,5);
-        console.log(year);
-        console.log(month);
-        console.log(day);
+
 
 
 
@@ -176,15 +309,85 @@ export default class EventInfo extends React.Component {
             })
         });
 
+    }
+
+    updateAverage(val) {
+        this.setState({
+            stars: val
+        })
 
 
     }
 
+    onButtonVoteEventPressed() {
+        this.setState({
+            press: true
+        })
+    }
+
+    onButtonVotePressed() {
+        let rootRef = firebase.database().ref('events/'+this.state.name);
+
+        let usVoted = rootRef.child('/votes');
+        let u=0;
+        usVoted.once('value').then(snapshot => {
+            u = snapshot.val()+1;
+            usVoted.set(u);
+            let av = rootRef.child('/averages');
+            av.once('value').then(aver => {
+                let averFinal = (aver.val()+this.state.stars)/u;
+                av.set(averFinal);
+            })
+        });
+        const { currentUser } = firebase.auth();
+        let email = currentUser.providerData[0].email;
+        let rootRef2 = firebase.database().ref();
+        let ref = rootRef2.child('users/');
+        ref.once('value').then(snapshot => {
+            snapshot.forEach(user => {
+                if (user.val().email === email) {
+                        let usF = ref.child(user.key+'/votedEvents');
+                        usF.push(this.state.name);
+                        this.setState({
+                            voted: true
+                        })
+                }
+            })
+        });
+
+    }
+
+
+
+
+
     render() {
 
+        if (this.state.before && !this.state.voted && !this.state.press) {
+            return(
+                <View style={styles.container}>
+                    <Text>DETAILS:</Text>
+                    <Text>Name: {this.state.name}</Text>
+                    <Text>Place: {this.state.place}</Text>
+                    <Text>Date: {this.state.date}</Text>
+                    <Text>Time: {this.state.time}</Text>
+                    <Text>Address: {this.state.address}</Text>
+                    <Text>City: {this.state.city}</Text>
+                    <Text>Country: {this.state.country}</Text>
+                    <Text
+                        onPress={this.onAddressClicked}
+                        style={styles.address}>Look for the address in the map</Text>
 
-        return (
-
+                    <TouchableOpacity
+                        style={styles.buttonVote}
+                        onPress={this.onButtonVoteEventPressed}
+                    >
+                        <Text style={{color: '#fff', fontSize: 25}}>Vote this event</Text>
+                    </TouchableOpacity>
+                </View>);
+        }
+        else if (this.state.before && !this.state.voted) {
+            return(
                 <View style={styles.container}>
                     <Text>DETAILS:</Text>
                     <Text>Name: {this.state.name}</Text>
@@ -205,11 +408,52 @@ export default class EventInfo extends React.Component {
                         half={true}
                         starSize={50}
                         spacing={10}
-                        fullStar={<Icon name={'star'} size={40} style={[styles.myStarStyle]}/>}
+                        fullStar={<Icon     name={'star'} size={40} style={[styles.myStarStyle]}/>}
                         emptyStar={<Icon name={'star-outline'} size={40} style={[styles.myStarStyle, styles.myEmptyStarStyle]}/>}
                         halfStar={<Icon name={'star-half'} size={40} style={[styles.myStarStyle]}/>}
                     />
                     <Text>{this.state.stars}</Text>
+                    <TouchableOpacity
+                        style={styles.buttonBooked}
+                        onPress={this.onButtonVotePressed}
+                    >
+                        <Text style={{color: '#fff', fontSize: 25}}>Vote</Text>
+                    </TouchableOpacity>
+                </View>
+            );
+        }
+        else if (this.state.before) {
+            return(
+                <View style={styles.container}>
+                    <Text>DETAILS:</Text>
+                    <Text>Name: {this.state.name}</Text>
+                    <Text>Place: {this.state.place}</Text>
+                    <Text>Date: {this.state.date}</Text>
+                    <Text>Time: {this.state.time}</Text>
+                    <Text>Address: {this.state.address}</Text>
+                    <Text>City: {this.state.city}</Text>
+                    <Text>Country: {this.state.country}</Text>
+                    <Text
+                        onPress={this.onAddressClicked}
+                        style={styles.address}>Look for the address in the map</Text>
+                    <Text>You have voted this event!</Text>
+                </View>
+            );
+        }
+        else {
+            return(
+                <View style={styles.container}>
+                    <Text>DETAILS:</Text>
+                    <Text>Name: {this.state.name}</Text>
+                    <Text>Place: {this.state.place}</Text>
+                    <Text>Date: {this.state.date}</Text>
+                    <Text>Time: {this.state.time}</Text>
+                    <Text>Address: {this.state.address}</Text>
+                    <Text>City: {this.state.city}</Text>
+                    <Text>Country: {this.state.country}</Text>
+                    <Text
+                        onPress={this.onAddressClicked}
+                        style={styles.address}>Look for the address in the map</Text>
                     <TouchableOpacity
                         style={styles[this.state.buttonStyle]}
                         onPress={this.onButtonPressed}
@@ -217,7 +461,9 @@ export default class EventInfo extends React.Component {
                         <Text style={{color: '#fff', fontSize: 25}}>{this.state.button}</Text>
                     </TouchableOpacity>
                 </View>
-        );
+            );
+        }
+
     }
 }
 
@@ -248,6 +494,15 @@ const styles = StyleSheet.create({
     buttonBooked: {
         alignItems: 'center',
         backgroundColor: '#0aa114',
+        marginTop: 15,
+        padding: 20,
+        borderRadius: 25,
+        margin: 40,
+
+    },
+    buttonVote: {
+        alignItems: 'center',
+        backgroundColor: '#cba008',
         marginTop: 15,
         padding: 20,
         borderRadius: 25,
